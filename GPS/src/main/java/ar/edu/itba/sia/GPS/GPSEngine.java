30d6411ed4ab84297806f0ec4f8b580902953b4c
jtorreguitar <jtorreguitar@itba.edu.ar>
2019-08-19 14:26:47 -0300

    added official classes and interfaces

---
+++ GPS/src/main/java/ar/edu/itba/sia/GPS/GPSEngine.java
@@ -0,0 +1,63 @@
+package ar.edu.itba.sia.gps;
+
+import java.util.Map;
+import java.util.Queue;
+
+import ar.edu.itba.sia.gps.api.Heuristic;
+import ar.edu.itba.sia.gps.api.Problem;
+import ar.edu.itba.sia.gps.api.State;
+
+public class GPSEngine {
+
+	Queue<GPSNode> open;
+	Map<State, Integer> bestCosts;
+	Problem problem;
+	long explosionCounter;
+	boolean finished;
+	boolean failed;
+	GPSNode solutionNode;
+	Heuristic heuristic;
+
+	protected SearchStrategy strategy;
+
+	public GPSEngine(Problem problem, SearchStrategy strategy, Heuristic heuristic) {
+	}
+
+	public void findSolution() {
+	}
+
+	// GETTERS FOR THE PEOPLE!
+
+	public Queue<GPSNode> getOpen() {
+		return open;
+	}
+
+	public Map<State, Integer> getBestCosts() {
+		return bestCosts;
+	}
+
+	public Problem getProblem() {
+		return problem;
+	}
+
+	public long getExplosionCounter() {
+		return explosionCounter;
+	}
+
+	public boolean isFinished() {
+		return finished;
+	}
+
+	public boolean isFailed() {
+		return failed;
+	}
+
+	public GPSNode getSolutionNode() {
+		return solutionNode;
+	}
+
+	public SearchStrategy getStrategy() {
+		return strategy;
+	}
+
+}
---
+++ GPS/src/main/java/ar/edu/itba/sia/GPS/GPSNode.java
@@ -0,0 +1,78 @@
+package ar.edu.itba.sia.gps;
+
+import ar.edu.itba.sia.gps.api.Rule;
+import ar.edu.itba.sia.gps.api.State;
+
+public class GPSNode {
+
+	private State state;
+	private GPSNode parent;
+	private Integer cost;
+	private Rule generationRule;
+	private Integer depth = 0;
+
+	public GPSNode(State state, Integer cost, Rule generationRule) {
+		this.state = state;
+		this.cost = cost;
+		this.generationRule = generationRule;
+	}
+
+	public GPSNode getParent() {
+		return parent;
+	}
+
+	public void setParent(GPSNode parent) {
+		this.parent = parent;
+		this.depth = parent.getDepth() + 1;
+	}
+
+	public State getState() {
+		return state;
+	}
+
+	public Integer getCost() {
+		return cost;
+	}
+
+	public Integer getDepth() {
+		return depth;
+	}
+
+	@Override
+	public String toString() {
+		return state.toString();
+	}
+
+	public String getSolution() {
+		if (this.parent == null) {
+			return this.state.toString();
+		}
+		return this.parent.getSolution() + this.state.toString();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		GPSNode other = (GPSNode) obj;
+		if (state == null) {
+			if (other.state != null)
+				return false;
+		} else if (!state.equals(other.state))
+			return false;
+		return true;
+	}
+
+	public Rule getGenerationRule() {
+		return generationRule;
+	}
+
+	public void setGenerationRule(Rule generationRule) {
+		this.generationRule = generationRule;
+	}
+
+}
