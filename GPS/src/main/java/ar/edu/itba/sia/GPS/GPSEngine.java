b97de476d2cf7f5ebaa1c44bdc0579daf59d409e
jtorreguitar <jtorreguitar@itba.edu.ar>
2019-08-20 20:34:06 -0300

    wrote parsing code

--- GPS/src/main/java/ar/edu/itba/sia/GPS/GPSEngine.java
+++ GPS/src/main/java/ar/edu/itba/sia/GPS/GPSEngine.java
@@ -1,61 +1,32 @@
 package ar.edu.itba.sia.GPS;
 
 import java.util.Map;
-import java.util.Queue;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.HashMap;
 
 import ar.edu.itba.sia.interfaces.*;
 
 public class GPSEngine {
 
-	Queue<GPSNode> open;
-	Map<State, Integer> bestCosts;
-	Problem problem;
-	long explosionCounter;
-	boolean finished;
-	boolean failed;
-	GPSNode solutionNode;
-	Heuristic heuristic;
+	private final Deque<GPSNode> open = new LinkedList<>();
+	private final Map<State, Integer> bestCosts = new HashMap<>();
+	private final Problem problem;
+	private long explosionCounter = 0;
+	private boolean finished = false;
+	private boolean failed = false;
+	private GPSNode solutionNode;
+	private final Heuristic heuristic;
 
 	protected SearchStrategy strategy;
 
-	public GPSEngine(Problem problem, SearchStrategy strategy, Heuristic heuristic) {
+	public GPSEngine(final Problem problem, final SearchStrategy strategy, final Heuristic heuristic) {
+        this.problem = problem;
+        this.strategy = strategy;
+        this.heuristic = heuristic;
 	}
 
 	public void findSolution() {
-	}
-
-	// GETTERS FOR THE PEOPLE!
-
-	public Queue<GPSNode> getOpen() {
-		return open;
-	}
-
-	public Map<State, Integer> getBestCosts() {
-		return bestCosts;
-	}
-
-	public Problem getProblem() {
-		return problem;
-	}
-
-	public long getExplosionCounter() {
-		return explosionCounter;
-	}
-
-	public boolean isFinished() {
-		return finished;
-	}
-
-	public boolean isFailed() {
-		return failed;
-	}
-
-	public GPSNode getSolutionNode() {
-		return solutionNode;
-	}
-
-	public SearchStrategy getStrategy() {
-		return strategy;
 	}
 
 }
---
+++ problem/src/main/java/ar/edu/itba/sia/problem/BoardParser.java
@@ -0,0 +1,71 @@
+package ar.edu.itba.sia.problem;
+
+import java.awt.Point;
+import com.google.gson.Gson;
+import com.google.gson.JsonParser;
+import java.io.*;
+import java.util.LinkedList;
+import java.util.Arrays;
+import java.io.FileNotFoundException;
+
+public class BoardParser {
+    
+    public SSState parse(String jsonFilePath) throws FileNotFoundException {
+        final FileReader reader = new FileReader(jsonFilePath);
+        final JsonParser parser = new JsonParser();
+        final JsonBoard jsonBoard = new Gson().fromJson(parser.parse(reader), JsonBoard.class);
+        final Square[] squares = parseSquares(jsonBoard);
+        final int[][] board = parseBoard(jsonBoard, squares);
+        return new SSState(board, squares);
+    }
+
+    private Square[] parseSquares(final JsonBoard board) {
+        LinkedList<Square> squares = new LinkedList<>();
+        for(int i = 0; i < board.blocks.length; i++)
+            squares.add(new Square(i,
+                                toPoint(board.blocks[i].goal),
+                                toPoint(board.blocks[i].initialPosition),
+                                board.blocks[i].direction));
+        return squares.toArray(new Square[] { });
+
+    }
+
+    private Point toPoint(JsonPoint point) {
+        return new Point(point.x, point.y);
+    }
+
+    private int[][] parseBoard(final JsonBoard jsonBoard, final Square[] squares) {
+        final int[][] board = emptyBoard(jsonBoard.size);
+        fillBoard(board, squares);
+        return board;
+    }
+
+    private void fillBoard(final int[][] board, final Square[] squares) {
+        for(Square square : squares)
+            board[square.getPosition().x][square.getPosition().y] = square.getId();
+    }
+
+    private int[][] emptyBoard(int size) {
+        final int[][] board = new int[size][size];
+        for(int i = 0; i < board.length; i++)
+            Arrays.fill(board[i], SSState.EMPTY_CELL);
+        return board;
+    }
+
+    private class JsonBoard {
+        int size;
+        JsonBlock[] blocks;
+        JsonPoint[] directionChangePoints;
+    }
+
+    private class JsonBlock {
+        JsonPoint initialPosition;
+        JsonPoint goal;
+        Direction direction;
+    }
+
+    private class JsonPoint {
+        int x;
+        int y;
+    }
+}
