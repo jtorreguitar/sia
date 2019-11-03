7d2e2e3530c7dfe2bdaf4307dcdc48986ac72fed
Sofia Picasso <spicasso@itba.edu.ar>
2019-11-03 16:39:02 -0300

    fixed roulette

--- TP3/GeneticAlgorithmGps/src/main/java/ar/edu/itba/sia/geneticAlgorithmGps/implementations/selectors/RouletteSelector.java
+++ TP3/GeneticAlgorithmGps/src/main/java/ar/edu/itba/sia/geneticAlgorithmGps/implementations/selectors/RouletteSelector.java
@@ -8,57 +8,45 @@ import java.util.*;
 
 public class RouletteSelector implements Selector {
 
-    private Random r;
+    private Random random;
     private int quantity;
 
-    public RouletteSelector (Random r, int quantity){
-        this.r = r;
+    public RouletteSelector (Random random, int quantity){
+        this.random = random;
         this.quantity = quantity;
     }
 
-    public List<Chromosome> select(List<Chromosome> chromosomes) {
+    @Override
+    public List<Chromosome> select(List<Chromosome> population) {
+       if (quantity % 2 != 0 )
+            quantity = quantity - 1;
 
-        int selectionCant = quantity;
-
-        //debe ser par
-        if (selectionCant % 2 != 0 )
-            selectionCant= selectionCant - 1;
-
-
-        List<Chromosome> selected = new LinkedList<>();
-        double accumToMatch[] = new double[selectionCant];
-
-        for (int i = 0 ; i < selectionCant; i++) {
-            double random = r.nextDouble();
-            accumToMatch[i] = random;
-        }
+        List<Chromosome> winners = new LinkedList<>();
 
         double totalFitness = 0;
-        for (Chromosome c : chromosomes)
+        for (Chromosome c : population)
             totalFitness += c.getAptitude();
 
-        // armar la ruleta
-        // Cada pair es el chromosome y el maximo double de su rango
-        List<Pair<Chromosome,Double>> roulette = new ArrayList<>();
-        double prevChromosomeAccum = 0;
-        for(int t = 0 ; t < chromosomes.size() ; t ++ ){
-            Chromosome currentChromosome = chromosomes.get(t);
-            double currentChromosomeAccum = prevChromosomeAccum + (currentChromosome.getAptitude() / totalFitness);
-            roulette.add(new Pair<>(currentChromosome, currentChromosomeAccum));
-            prevChromosomeAccum = currentChromosomeAccum;
-        }
+        int i = 0;
 
-        int j = 0;
-        for (int i = 0; i < accumToMatch.length ; i ++) {
-            if( accumToMatch[i] < roulette.get(j).getValue() ){
-                selected.add( roulette.get(j).getKey() );
-                i++;
-                j = 0;
-            } else {
-                j++;
+        while(i < quantity){
+
+            i++;
+
+            double pickWinner = random.nextDouble();;
+
+            double accumulated = 0;
+
+            int t = 0;
+
+            while(accumulated + (population.get(t).getAptitude())/totalFitness < pickWinner){
+                accumulated += (population.get(t).getAptitude())/totalFitness;
+                t++;
             }
-        }
 
-        return selected;
+            winners.add(population.get(t));
+
+        }
+        return winners;
     }
 }
